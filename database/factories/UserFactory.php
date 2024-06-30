86d796c333c98df3803f63b59bacc902b9954a7c
Andreas Klebe <a.klebe.work@gmail.com>
2024-06-30 17:29:06 +0200

    - User password implicit hashed

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,7 @@ class UserFactory extends Factory // \database\factories\UserFactory
             'email'             => fake()->unique()->safeEmail(),
             'shared_id'         => uniqid('js_suid_'),
             'email_verified_at' => now(),
-            'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'password'          => '1234567',
             // decoded: "password"
             'remember_token'    => Str::random(10),
         ];
