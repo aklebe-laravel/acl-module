fe9bfdb6118e0ca8a221a634d51fc18b83bdf42a
Andreas Klebe <a.klebe.work@gmail.com>
2024-12-03 09:59:01 +0100

    - seeder - prepared import

---
+++ app/Http/Livewire/Form/ImportAclGroup.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Modules\Acl\app\Http\Livewire\Form;
+
+use Modules\Form\app\Http\Livewire\Form\Base\NativeObjectBase;
+
+class ImportAclGroup extends NativeObjectBase
+{
+}
---
+++ config/seeders.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'acl_groups'    => [
+        'count' => 10,
+    ],
+    'acl_resources' => [
+        'count' => 20,
+    ],
+];
