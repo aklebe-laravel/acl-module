30b16065ff09e0f0b0f477e3197e83aee0255a21
Andreas Klebe <a.klebe.work@gmail.com>
2025-01-11 21:52:50 +0100

    added users to form AclGroup

---
+++ app/Models/AclGroupAclResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Modules\Acl\app\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @mixin IdeHelperAclGroupUser
+ */
+class AclGroupAclResource extends Model
+{
+    /**
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
+     * @var string
+     */
+    protected $table = 'acl_group_acl_resource';
+
+    //    /**
+//     * You can use this instead of newFactory()
+//     * @var string
+//     */
+//    public static string $factory = AclGroupUserFactory::class;
+
+}
